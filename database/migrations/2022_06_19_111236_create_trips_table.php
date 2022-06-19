9e27a7a20e47042494676041666145958df40c97
aciernoantonio <aciernoantonio.int@gmail.com>
2022-06-19 13:23:19 +0200

    add trips migration to mysql db

---
+++ database/migrations/2022_06_19_111236_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('city', 30);
+            $table->tinyInteger('flight_duration');
+            $table->string('image');
+            $table->decimal('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
